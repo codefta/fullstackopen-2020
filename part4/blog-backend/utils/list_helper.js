8628b760e0b76e4ea4c8d84a25e05341f862eabd
fathisiddiqi <service.fathisiddiqi@gmail.com>
2020-04-16 15:49:16 +0700

    add part 4.4

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  const reducer = (value, totalValue) => value + totalValue
+
+  return blogs.map((b) => b.likes).reduce(reducer, 0)
+}
+
 module.exports = {
   dummy,
+  totalLikes,
 }
