93668419bd4c50c13d71122f7a0694434c6b76da
fathisiddiqi <service.fathisiddiqi@gmail.com>
2020-04-16 18:40:16 +0700

    4.6 & 4.7

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -17,8 +19,55 @@ const favoriteBlog = (blogs) => {
   return findBlog
 }
 
+const mostBlogs = (blogs) => {
+  const authorGroupped = _(blogs)
+    .groupBy('author')
+    .map((a, i) => {
+      return {
+        author: i,
+        blogs: a.length,
+      }
+    })
+    .values()
+    .value()
+
+  const reducer = (maxValue, totalValue) => Math.max(maxValue, totalValue)
+
+  const mostBlog = authorGroupped.map((a) => a.blogs).reduce(reducer, -Infinity)
+
+  const findAuthor = authorGroupped.find((a) => a.blogs === mostBlog)
+  return findAuthor
+}
+
+const mostLikes = (blogs) => {
+  const reducer = (value, totalValue) => value + totalValue
+
+  const authorGroupped = _(blogs)
+    .groupBy('author')
+    .map((a, i) => {
+      return {
+        author: i,
+        likes: a.map((b) => b.likes).reduce(reducer, 0),
+      }
+    })
+    .values()
+    .value()
+
+  const reducer2 = (maxValue, totalValue) => Math.max(maxValue, totalValue)
+
+  const mostLikes = authorGroupped
+    .map((a) => a.likes)
+    .reduce(reducer2, -Infinity)
+
+  const findAuthor = authorGroupped.find((a) => a.likes === mostLikes)
+
+  return findAuthor
+}
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
+  mostLikes,
 }
