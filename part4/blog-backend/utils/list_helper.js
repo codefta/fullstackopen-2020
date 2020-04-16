24c9f16975558325f583b6156be60c05a7507c93
fathisiddiqi <service.fathisiddiqi@gmail.com>
2020-04-16 16:08:30 +0700

    add part 4.5

--- part4/blog-backend/utils/list_helper.js
+++ part4/blog-backend/utils/list_helper.js
@@ -8,7 +8,17 @@ const totalLikes = (blogs) => {
   return blogs.map((b) => b.likes).reduce(reducer, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  const reducer = (maxValue, totalValue) => Math.max(maxValue, totalValue)
+
+  const maxLikes = blogs.map((b) => b.likes).reduce(reducer, -Infinity)
+
+  const findBlog = blogs.find((b) => b.likes === maxLikes)
+  return findBlog
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
