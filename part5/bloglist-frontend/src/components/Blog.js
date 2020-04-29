48b7603d408a1d267cef5d1cd95cbaf76c9c3f5c
fathisiddiqi <service.fathisiddiqi@gmail.com>
2020-04-29 10:51:22 +0700

    add part 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,52 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState, useEffect } from 'react'
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+  const [visible, setVisible] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  useEffect(() => {
+    const userLoad = blog.user
+    if (userLoad) {
+      setUser(userLoad)
+    }
+  }, [blog.user])
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  console.log(blog)
+
+  console.log(user)
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        <div>
+          {blog.title} {blog.author}{' '}
+          <button onClick={toggleVisibility}>hide</button>
+        </div>
+        <div>{blog.url}</div>
+        <div>
+          {blog.likes} <button>like</button>
+        </div>
+        <div>{user ? user.name : ''}</div>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
